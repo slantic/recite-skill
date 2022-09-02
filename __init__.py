9994a48b7438c15632e1bf55ee06a503cb0fc671
Slantic <pascalvannugteren@gmail.com>
2022-09-02 19:52:47 +0200

    Added list feature

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
 
-
 class Recite(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
@@ -8,13 +7,22 @@ class Recite(MycroftSkill):
     @intent_file_handler('recite.intent')
     def handle_recite(self, message):
         poem = message.data.get('poem')
-        text = ''
+        with self.file_system.open('poems/' + poem.replace(" ", "_"), "r") as f:
+            text = f.read()
 
         self.speak_dialog('recite', data={
             'poem': poem,
             'text': text
         })
 
+    @intent_file_handler('list.intent')
+    def handle_list(self):
+        with self.file_system.open('poems/list.txt', "r") as f:
+            poems_list = f.read()
+
+        self.speak_dialog('list', data={
+            'poems_list': poems_list
+        })
 
 def create_skill():
     return Recite()
